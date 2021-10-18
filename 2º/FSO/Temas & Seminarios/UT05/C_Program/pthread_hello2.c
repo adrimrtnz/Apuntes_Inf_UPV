342c1ea8b7938c883a1f68b3c5037c9cf46735fa
Adrián Martínez <adrimrtnz@gmail.com>
2021-10-18 08:04:07 +0200

    added *.c files

---
+++ 2º/FSO/Temas & Seminarios/UT05/C_Program/pthread_hello2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+ 
+void *My_Print (void *ptr) 
+{
+    char *message;
+
+    message = (char *) ptr;
+    write(1, message, strlen(message));
+}
+
+int main() 
+{ 
+    pthread_t pthread1, pthread2;
+    pthread_attr_t attr;
+  
+    printf("Main thread --> creating threads \n");
+    pthread_attr_init(&attr);
+    pthread_create(&pthread1, &attr, My_Print, "Hello");
+    pthread_create(&pthread2, &attr, My_Print, "World");
+    printf("Main thread --> I am sleeping for 5 seconds... \n");
+    sleep(5);
+
+    return(0); 
+
+}
