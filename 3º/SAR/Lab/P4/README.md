ce185c80038bbfd6094c7df7cfd6a12644515ed7
Adrián Martínez <adrimrtnz@gmail.com>
2023-05-24 08:05:28 +0200

    Material Lab PER y SAR

---
+++ 3º/SAR/Lab/P4/README.md
@@ -0,0 +1,9 @@
+# Proyecto SAR 2023
+
+Proyecto de la asignatura Sistemas de Almacenamiento y Recuperación de la información (SAR) del grado de Ingeniería Informática, rama de computación, de la Univarsidad Politécnica de Valencia (UPV).
+
+## Integrantes
+* Jorge Baeza García
+* Pablo García López
+* Aleksandra Loginova
+* Adrián Martínez Martínez
--- 3º/SAR/Lab/P4/SAR_Crawler_lib.py
+++ 3º/SAR/Lab/P4/SAR_Crawler_lib.py
@@ -146,33 +146,115 @@ class SAR_Wiki_Crawler:
         else:
             document = {}
         
-        groups = match.groupdict()              # Diccionario con las keys ['title', 'summary', 'rest']
+        # Diccionario con las keys ['title', 'summary', 'rest']
+        groups = match.groupdict()
         
         document['url'] = url
         document['title'] = groups['title']
         document['summary'] = groups['summary']
-        document['sections'] = {}
+        document['sections'] = []
 
         sections = self.sections_re.findall(groups['rest'])
-        
+
         # Creación de tuplas (ini_section, ini_next_section)
         section_tuples_list = []
         for i in range(len(sections) - 1):
             section_tuples_list.append(tuple(sections[i:i+2]))
-        
-        # TODO refactorizar esto en método para sección
+
+
         for s in section_tuples_list:
             ini = text.find(s[0])
             fin = text.find(s[1])
             
             section_text = text[ini:fin]
-            
-            # Extraemos las partes de la sección
-            match = self.section_re.match(section_text)
-            section_dic = match.groupdict()     # Diccionario con las keys ['title', 'summary', 'rest']
-            
+            self.extract_section_data(document, section_text, s[1])
 
+        # En el bucle de tuplas se queda fuera la tupla (last_section, fin)
+        if len(section_tuples_list) > 1:
+            s = section_tuples_list[-1][1]
+            ini = text.find(s)
+            
+            section_text = text[ini:]
+            self.extract_section_data(document, section_text, None)    
+            
         return document
+
+
+    def extract_section_data(self, document: dict, text: str, next_section_ini: str):
+        """
+        Extrae la información de la sección y la añade al documento
+
+        Args:
+            document (dict): diccionario del documento al que pertenece la sección
+            text (str): texto de la sección
+            next_section_ini (str): texto para delimitar el fin de esta sección
+        """
+
+        # Extraemos las partes de la sección
+        match = self.section_re.match(text)
+        # Diccionario con las keys ['name', 'text', 'rest']
+        section_dic = match.groupdict()   
+        
+        section = {}
+        section['name'] = section_dic['name']
+        section['text'] = section_dic['text']
+        section['subsections'] = []
+        document['sections'].append(section)
+
+        # Subsecciones
+        subsections = self.subsections_re.findall(section_dic['rest'])
+
+        # Creación de tuplas (ini_subsection, ini_next_subsection)
+        subsections_tuples_list = []
+        for j in range(len(subsections) - 1):
+            subsections_tuples_list.append(tuple(subsections[j:j+2]))
+        
+        # Si hay subsecciones, en el bucle anterior se deja fuera la última subsección
+        # la añadimos aquí delimitándola con el next_section_ini
+        if len(subsections_tuples_list) > 1:
+            t = (subsections_tuples_list[-1][1],next_section_ini)
+            subsections_tuples_list.append(t)
+
+        # Si solo hay una subsección no se crea ninguna tupla en los bucles anteriores
+        if len(subsections) > 0 and len(subsections_tuples_list) == 0:
+            subsections_tuples_list.append((subsections[0], next_section_ini))
+
+        for sub in subsections_tuples_list:
+            ini = text.find(sub[0])
+
+            if sub[1] is not None:
+                fin = text.find(sub[1])
+            
+            if fin is not None:
+                subsection_text = text[ini:fin]
+            else:
+                subsection_text = text[ini:]
+            self.extract_subsection_data(section, subsection_text)
+
+
+    def extract_subsection_data(self, section: dict, text: str):
+        """
+        Extrae la información de la subsección y la añade a la sección
+
+        Args:
+            section (dict): diccionario de la sección a la que pertenece la subsección
+            text (str): texto de la subsección
+
+        en caso de no encontrar subsecciones de la sección, devolverá None
+        """
+        
+        # Extraemos las partes de cada subsección si las tiene
+        match = self.subsection_re.match(text)
+        if match is None:
+            return None
+        
+        # Diccionario con las keys ['name', 'text']
+        subsection_dic = match.groupdict()
+
+        subsection = {}
+        subsection['name'] = subsection_dic['name']
+        subsection['text'] = subsection_dic['text']
+        section['subsections'].append(subsection)
 
 
     def save_documents(self,
@@ -250,23 +332,55 @@ class SAR_Wiki_Crawler:
             # Suponemos que vamos a poder alcanzar el límite para la nomenclatura
             # de guardado
             total_files = math.ceil(document_limit / batch_size)
+            print(f'Tengo batch_size de {batch_size}, con un total_files={total_files}, y document_limit={document_limit}')
 
-        # COMPLETAR
-        # 1. Seleccionar una página no procesada de la cola de prioridad
-        url = hq.heappop(queue)[2] # Esto devuelve una tupla de la que la url es el tercer componente
+        while len(queue) > 0 and (total_documents_captured < document_limit):
 
-        # 2. Descarga el contenido textual de la página y los enlaces que aparecen en ella
-        content = self.get_wikipedia_entry_content(url)[0]
+            # 1. Seleccionar una página no procesada de la cola de prioridad
+            depth, parent_url, url = hq.heappop(queue)
+            # print(f'Voy a mirar url: {url}, y url padre: {parent_url}')
 
-        # 3. Añadir, si procede, los enlaces a la cola de páginas pendientes de procesar
-            # no se ha visitado
-            # es url valida
-            # hemos llegado a profundidad máxima?
-        
-        # 4. Analizar el contenido textual para generar el diccionario con el contentenido estructurado del artículo
-        # TODO lo de abajo es temporal, seguramente no se guarde así
-        document = self.parse_wikipedia_textual_content(content, url)
-        self.save_documents([document], base_filename)
+            # 2. Descarga el contenido textual de la página y los enlaces que aparecen en ella
+            test_url = self.get_wikipedia_entry_content(url)
+            visited.add(url)
+
+            if test_url is not None:
+                content, urls = test_url
+                
+                # Comentar o descomentar para comprobar lo que se coge realmente
+                # with open(f'content-{total_documents_captured}.txt', 'w', encoding='utf-8') as content_f:
+                #     print(content, file=content_f)
+                # with open(f'urls-{total_documents_captured}.txt', 'w', encoding='utf-8') as urls_f:
+                #     print(urls, file=urls_f)
+
+                document = self.parse_wikipedia_textual_content(content, url)
+                if document is None:
+                    continue
+
+                total_documents_captured += 1
+                documents.append(document)
+
+                if depth < max_depth_level:
+                    for child_url in urls:
+                        if self.is_valid_url(child_url):
+                            if not 'http' in child_url:
+                                child_url = urljoin(url, child_url)
+                            if child_url not in visited:
+                                queue.append((depth+1, url, child_url))
+
+            
+                
+                if (batch_size is not None and len(documents) == batch_size):
+                    self.save_documents(documents, base_filename, files_count+1, total_files);
+                    files_count += 1
+                    documents = []
+                    # print(f'He guardado la parte ({files_count}/{total_files})')
+
+        # hay que pasarle los cuatro parámetros, comprobar si hay documentos que guardar por si batch_size
+        # no dejar documentos sin guardar, si no batch_size, la siguiente instrucción guarda todo
+        if (len(documents) > 0):    
+            files_count += 1
+            self.save_documents(documents, base_filename, files_count, total_files)
 
 
 
